0e82dbf274b6ca76513eb78e2467cfff03ac2a64
Padstone <62522808+Padstone@users.noreply.github.com>
2022-02-03 12:15:15 +1100

    Add files via upload

---
+++ StockMate/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿// (c) Copyright Skillage I.T.
+// (c) This file is Skillage I.T. software and is made available under license.
+// (c) This software is developed by: Michael Padworth
+// (c) Date: 28th Jan 2021 Time: 11:34
+// (c) File Name: Program.cs Version: 1-0
+
+namespace StockMate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.saveAsButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ItemCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ItemDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CurrentCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OnOrder = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.exportXML = new System.Windows.Forms.Button();
+            this.xmlTheme = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(119, 39);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(363, 39);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // saveAsButton
+            // 
+            this.saveAsButton.Location = new System.Drawing.Point(621, 39);
+            this.saveAsButton.Name = "saveAsButton";
+            this.saveAsButton.Size = new System.Drawing.Size(75, 23);
+            this.saveAsButton.TabIndex = 2;
+            this.saveAsButton.Text = "Save As";
+            this.saveAsButton.UseVisualStyleBackColor = true;
+            this.saveAsButton.Click += new System.EventHandler(this.saveAsButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ItemCode,
+            this.ItemDescription,
+            this.CurrentCount,
+            this.OnOrder});
+            this.dataGridView1.Location = new System.Drawing.Point(119, 150);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.Size = new System.Drawing.Size(577, 274);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // ItemCode
+            // 
+            this.ItemCode.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ItemCode.HeaderText = "Item Code";
+            this.ItemCode.Name = "ItemCode";
+            this.ItemCode.ReadOnly = true;
+            // 
+            // ItemDescription
+            // 
+            this.ItemDescription.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ItemDescription.HeaderText = "Item Description";
+            this.ItemDescription.Name = "ItemDescription";
+            this.ItemDescription.ReadOnly = true;
+            // 
+            // CurrentCount
+            // 
+            this.CurrentCount.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.CurrentCount.HeaderText = "Current Count";
+            this.CurrentCount.Name = "CurrentCount";
+            // 
+            // OnOrder
+            // 
+            this.OnOrder.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.OnOrder.HeaderText = "On Order";
+            this.OnOrder.Name = "OnOrder";
+            // 
+            // exportXML
+            // 
+            this.exportXML.Location = new System.Drawing.Point(702, 281);
+            this.exportXML.Name = "exportXML";
+            this.exportXML.Size = new System.Drawing.Size(91, 23);
+            this.exportXML.TabIndex = 4;
+            this.exportXML.Text = "Export to XML";
+            this.exportXML.UseVisualStyleBackColor = true;
+            this.exportXML.Click += new System.EventHandler(this.exportXML_Click);
+            // 
+            // xmlTheme
+            // 
+            this.xmlTheme.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.xmlTheme.FormattingEnabled = true;
+            this.xmlTheme.Items.AddRange(new object[] {
+            "Dark Theme",
+            "Light Theme"});
+            this.xmlTheme.Location = new System.Drawing.Point(702, 254);
+            this.xmlTheme.Name = "xmlTheme";
+            this.xmlTheme.Size = new System.Drawing.Size(91, 21);
+            this.xmlTheme.TabIndex = 5;
+            this.xmlTheme.SelectedIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 474);
+            this.Controls.Add(this.xmlTheme);
+            this.Controls.Add(this.exportXML);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.saveAsButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "StockMate Stock Clearing Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button saveAsButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ItemCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ItemDescription;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CurrentCount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OnOrder;
+        private System.Windows.Forms.Button exportXML;
+        private System.Windows.Forms.ComboBox xmlTheme;
+    }
+}
+
---
+++ StockMate/Form1.cs
@@ -0,0 +1,354 @@
+﻿// (c) Copyright Skillage I.T.
+// (c) This file is Skillage I.T. software and is made available under license.
+// (c) This software is developed by: Michael Padworth
+// (c) Date: 28th Jan 2021 Time: 11:34
+// (c) File Name: Program.cs Version: 1-0
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.VisualBasic.FileIO;
+
+namespace StockMate
+{   
+    public partial class Form1 : Form
+    {
+        //Class variables that are used throughout the runtime.
+        string filePath = string.Empty;
+        int totalRow = 0;
+        
+        //Constructor. Do not change.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Behaviour for the "Open" button
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            //Used to open a open-file window
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            //Set the initial folders and specify the types of file to be opened.
+            openFileDialog.InitialDirectory = "c:\\stockFiles";
+            openFileDialog.Filter = "csv files (*.csv)|*.csv";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+            
+            //If the dialogue is successfully opened
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                filePath = openFileDialog.FileName;
+                try
+                {
+                    open(openFileDialog.FileName);
+
+                } catch (Exception)
+                {
+                    //In the event of unexpected error
+                    MessageBox.Show($"File failed to open. Please try again.");
+                }
+            }
+            else
+            {
+                //In the event of unexpected error
+                MessageBox.Show($"Could not initialise a dialogue box. Please try again.");
+            }
+        }
+
+        //Behaviour for the "Save" button.
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            //If there is more than one row in the data grid, i.e. there is data in the data grid
+            if(totalRow > 1)
+            {
+                //Call the save function with the original file's name.
+                save(filePath);
+            }
+            //Otherwise
+            else
+            {
+                //Call the NoFile function.
+                NoFile();
+            }
+        }
+
+        //Behaviour for the "Save As" button
+        private void saveAsButton_Click(object sender, EventArgs e)
+        {
+            ////If there is more than one row in the data grid, i.e. there is data in the data grid
+            if (totalRow > 1)
+            {
+                //Using integrated Visual Basic feature to prompt the user to input a new file name
+                string newFileAddress = Microsoft.VisualBasic.Interaction.InputBox("Please input a new file name without the extension.", "New name", "");
+                
+                //If the file name is empty
+                if(newFileAddress != "")
+                {
+                    //Check for special characters not allowed in Windows 10
+                    if(newFileAddress.Contains('"') || newFileAddress.Contains('?') || newFileAddress.Contains('<') || newFileAddress.Contains('>') || newFileAddress.Contains('*') || newFileAddress.Contains(':') || newFileAddress.Contains('|') || newFileAddress.Contains('/') || newFileAddress.Contains('\\'))
+                    {
+                        MessageBox.Show($"Invalid file name. File name must not contain any of \\ / : * ? \" < > |. Please try again.");
+                        saveAsButton_Click(sender, e);
+                    }
+
+                    //If the file name is valid
+                    else
+                    {
+                        //Format the file name
+                        newFileAddress = "c:\\stockFiles\\" + newFileAddress + ".csv";
+
+                        //Used to decide whether there exists any file with the same name, and whether the user wants to override the file
+                        bool confirm = true;
+
+                        //Check if a file with the same name exists
+                        if (File.Exists(@newFileAddress))
+                        {
+                            //Prompt the user for overriding option
+                            DialogResult dialogResult = MessageBox.Show("A file with the same name already exists. Override the file?", "Override?", MessageBoxButtons.YesNo);
+                            
+                            //If the user does not want to override the existing file
+                            if (dialogResult == DialogResult.No)
+                            {
+                                confirm = false;
+                            }
+                        }
+
+                        //If there is no file with the same name, or if the user wants to override the existing file
+                        if (confirm)
+                        {
+                            //Calls the save function with the new name
+                            save(newFileAddress);
+                        }
+                        else
+                        {
+                            //Prompts the user again for a new file name
+                            saveAsButton_Click(sender, e);
+                        }
+                    }
+
+                }
+                else
+                {
+                    //If the user didn't specify a file name
+                    MessageBox.Show($"No file name specified. Please try again.");
+                }
+            }
+
+            //If there is no data.
+            else
+            {
+                NoFile();
+            }
+
+        }
+
+        public void open(string fileName)
+        {
+            //Used to store data read from the file.
+            string fileContent = string.Empty;
+
+            //Read the contents of the file into a stream
+            //var fileStream = openFileDialog.OpenFile();
+            Stream fileStream = File.OpenRead(fileName);
+
+
+            //If the file is successfully opened, clear the existing displayed data (if any).
+            totalRow = 0;
+            dataGridView1.Rows.Clear();
+
+            //Reads the entire document.
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                //Skip the first line (with the columns name)
+                reader.ReadLine();
+
+                //Read the rest of the data
+                fileContent = reader.ReadToEnd();
+                reader.Close();
+            }
+
+            // This splits the file's content by new lines.
+            string[] lines = fileContent.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            // This is supposed to be (lines.Length - 1), as the file provided has an empty line at the end of the file.
+            // But the dataGridView object in the form also contains an extra line for each field's name by design which negates the -1.
+            // Therefore the -1 is not included here to avoid an unnecessary totalRow++.
+            totalRow = lines.Length;
+
+            //Split each line into 4 parts according to the design
+            foreach (string a in lines)
+            {
+                string[] con = a.Split(',');
+                if (con.Length == 4)
+                {
+                    //Load the data to the data grid.
+                    //con[2] is parsed into an integer so the built-in sorting functions would treat it as a numeric value
+                    dataGridView1.Rows.Add(con[0], con[1], Int32.Parse(con[2]), con[3]);
+                }
+            }
+        }
+
+        //Function used by "Save" and "Save As" buttons. Performs the saving function.
+        public void save(string filename)
+        {
+            //Using try in case of errors
+            try
+            {
+                //Initialising the output file stream with the file name passed to this function.
+                System.IO.StreamWriter toFile = new System.IO.StreamWriter(filename);
+
+                //Writes the first line (the columns name)
+                toFile.WriteLine("Item Code,Item Description,Current Count,On Order");
+
+                //Count the total number of columns
+                int columnCount = dataGridView1.Columns.Count;
+
+                //Write each row into the file according to the format of a .csv file
+                for (int i = 1; i <= dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < columnCount-1; j++)
+                    {
+                        toFile.Write(dataGridView1.Rows[i - 1].Cells[j].Value.ToString() + ",");
+                    }
+                    toFile.WriteLine(dataGridView1.Rows[i - 1].Cells[columnCount - 1].Value.ToString());
+                }
+
+                //Close the stream
+                toFile.Close();
+
+                //Also sets the current opening file to the new file created.
+                //Not putting this under the buttons behaviour in case of unexpected errors in the save process.
+                filePath = filename;
+
+                //Telling the user the process is successful.
+                MessageBox.Show($"File successfully saved to {filename}.");
+            }
+            catch (Exception)
+            {
+                //In case error happens.
+                MessageBox.Show($"File failed to save. Please try again.");
+            }
+        }
+
+        //If there is no data in the data grid but the user tries to save the data into a file
+        public void NoFile()
+        {
+            //Shows a message.
+            MessageBox.Show("No file opened. Please open a file before performing this action.");
+        }
+
+        private void exportXML_Click(object sender, EventArgs e)
+        {
+            if (totalRow <= 1)
+            {
+                NoFile();
+            }
+            else
+            {
+                export(this.xmlTheme.SelectedIndex);
+                Process.Start("msedge", "c:\\stockFiles\\exportedXML.xml");
+            }
+        }
+
+        private void export(int style)
+        {
+            int columnCount = dataGridView1.Columns.Count;
+            System.IO.StreamWriter toFile = new System.IO.StreamWriter("c:\\stockFiles\\exportedXML.xml");
+            toFile.WriteLine("<?xml version=\"1.0\"?>");
+            if(style == 0)
+            {
+                toFile.WriteLine("<?xml-stylesheet type=\"text/css\" href=\"dark.css\"?>");
+                darkCSS();
+            }
+            else
+            {
+                toFile.WriteLine("<?xml-stylesheet type=\"text/css\" href=\"light.css\"?>");
+                lightCSS();
+            }
+            toFile.WriteLine("<xs:schema xmlns:xs = \"http://www.w3.org/2001/XMLSchema\">");
+            toFile.WriteLine("	<xs:element name=\"stocks\">");
+            toFile.WriteLine("	  <xs:complexType>");
+            toFile.WriteLine("		<xs:sequence>");
+            toFile.WriteLine("		  <xs:element name=\"heading\" type=\"xs: string\"/>");
+            toFile.WriteLine("		  <xs:element name=\"stock\" type=\"xs: string\"/>");
+            toFile.WriteLine("		</xs:sequence>");
+            toFile.WriteLine("	  </xs:complexType>");
+            toFile.WriteLine("	</xs:element>");
+
+            toFile.WriteLine("	<stocks>");
+            toFile.WriteLine("    <heading>Item Code,Item Description,Current Count,On Order</heading>");
+            for (int i = 1; i <= dataGridView1.Rows.Count; i++)
+            {
+                toFile.Write("        <stock>");
+                for (int j = 0; j < columnCount - 1; j++)
+                {
+                    toFile.Write(dataGridView1.Rows[i - 1].Cells[j].Value.ToString() + ",");
+                }
+                toFile.Write(dataGridView1.Rows[i - 1].Cells[columnCount - 1].Value.ToString());
+                toFile.WriteLine("</stock>");
+            }
+            toFile.WriteLine("	</stocks>");
+            toFile.WriteLine("</xs:schema>");
+            toFile.Close();
+        }
+
+        public void darkCSS()
+        {
+            System.IO.StreamWriter toFile = new System.IO.StreamWriter("c:\\stockFiles\\dark.css");
+            toFile.WriteLine("books { ");
+            toFile.WriteLine("     color: white; ");
+            toFile.WriteLine("     background-color : black; ");
+            toFile.WriteLine("     width: 100%; ");
+            toFile.WriteLine("} ");
+            toFile.WriteLine(" heading { ");
+            toFile.WriteLine("     color: white; ");
+            toFile.WriteLine("     font-size : 40px; ");
+            toFile.WriteLine("     background-color : gray; ");
+            toFile.WriteLine("} ");
+            toFile.WriteLine(" heading, title, author, publisher, edition, price, stock { ");
+            toFile.WriteLine("     display : block; ");
+            toFile.WriteLine("} ");
+            toFile.WriteLine(" title { ");
+            toFile.WriteLine("     font-size : 25px; ");
+            toFile.WriteLine("     font-weight : bold; ");
+            toFile.WriteLine("} ");
+            toFile.WriteLine(" stock { ");
+            toFile.WriteLine("     background-color : black; ");
+            toFile.WriteLine("     color: white; ");
+            toFile.WriteLine("} ");
+            toFile.Close();
+        }
+
+        public void lightCSS()
+        {
+            System.IO.StreamWriter toFile = new System.IO.StreamWriter("c:\\stockFiles\\light.css");
+            toFile.WriteLine("books { ");
+            toFile.WriteLine("     color: black; ");
+            toFile.WriteLine("     background-color : white; ");
+            toFile.WriteLine("     width: 100%; ");
+            toFile.WriteLine("} ");
+            toFile.WriteLine(" heading { ");
+            toFile.WriteLine("     color: black; ");
+            toFile.WriteLine("     font-size : 40px; ");
+            toFile.WriteLine("     background-color : LightGray; ");
+            toFile.WriteLine("} ");
+            toFile.WriteLine(" heading, title, author, publisher, edition, price, stock { ");
+            toFile.WriteLine("     display : block; ");
+            toFile.WriteLine("} ");
+            toFile.WriteLine(" title { ");
+            toFile.WriteLine("     font-size : 25px; ");
+            toFile.WriteLine("     font-weight : bold; ");
+            toFile.WriteLine("} ");
+            toFile.Close();
+        }
+    }
+}
